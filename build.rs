619b2cc5a737c5d9cc337e1bcd98fb77ddd610a8
zg <zangge885@gmail.com>
2026-07-06 22:09:48 +0800

    feat: embed Windows application icon & append version number to GitHub release filenames

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
+}
